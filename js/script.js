0544f73263c97b099f577d73fe3869231ec3c8df
Sajjad-Kazemi <sajjad9410kazemi@gmail.com>
2022-03-14 20:15:08 +0330

    only return left

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const $ = document
 const wheel = $.querySelector('#wheel-container')
-const insertList = $.querySelector('#insertInfo')
 const numbers = $.querySelectorAll('.info div')
+const insertList = $.querySelector('#insertInfo')
+const insertInfo = $.querySelectorAll('#insertInfo input')
+
+
 
 numbers.forEach(item =>{
   item.addEventListener('click',(e)=>{
@@ -16,5 +19,62 @@ numbers.forEach(item =>{
       numbers[i].className = 'active'
       numbers[i].nextElementSibling.disabled = false
     }
+    changeWheel(e.target.textContent)
   })
+})
+
+const changeWheel = (num) => {
+  for (let i of wheel.children){
+    if(i.id === 'rotateBtn'){
+      continue
+    }
+    i.remove()
+  }
+  if(num == 1){
+    const option = $.createElement('div')
+    option.id= 'option1'
+    option.className= 'oneOfone'
+    option.textContent = insertInfo[0].value
+    wheel.appendChild(option)
+  } else if(num == 2){
+    makeOption(num,'oneOftwo').forEach(item =>{
+      wheel.appendChild(item)
+    })
+  } else if(num == 3){
+    makeOption(num,'oneOfthree').forEach(item =>{
+      wheel.appendChild(item)
+    })
+  } else if(num == 4){
+    makeOption(num,'oneOffour').forEach(item =>{
+      wheel.appendChild(item)
+    })
+  } else if(num == 5){
+    makeOption(num,'oneOffive').forEach(item =>{
+      wheel.appendChild(item)
+    })
+  } else if(num == 6){
+    makeOption(num,'oneOfsix').forEach(item =>{
+      wheel.appendChild(item)
+    })
+  }else(alert('Error!'))
+}
+
+const makeOption = (num,cls) =>{
+  let elemList = []
+  for (let i = 0;i<num;i++){
+    const option = $.createElement('div')
+    option.id = 'option' + (i + 1).toString()
+    option.className = cls
+    option.textContent = insertInfo[i].value
+    elemList.push(option)
+  }
+  return elemList
+}
+
+insertInfo.forEach((item,index) =>{
+  item.addEventListener('input', e =>{
+    const target = e.target
+    const dist = $.getElementById('option'+(index+1))
+    dist.textContent = target.value
+    })
 })
